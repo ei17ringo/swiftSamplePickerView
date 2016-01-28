5980c58b1859e09ff84b92517c9cedb2e514c0c2
ei17ringo <ei17ringo@gmail.com>
2016-01-28 18:02:21 +0800

    commit init

--- samplePickerView/ViewController.swift
+++ samplePickerView/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
+    @IBOutlet weak var myPicker: UIPickerView!
+    
+    // データを配列で用意する
+    var tea_list = ["ダージリン","アールグレイ","アッサム","オレンジペコ"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myPicker.dataSource = self
+        myPicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // ピッカービューの列数
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // ピッカービューの行数
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tea_list.count
+    }
 
+    // ピッカービューに表示する文字
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tea_list[row]
+    }
+    
+    //  ピッカービューで選択された時に行う処理
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("選択されたのは\(row)行目で\(tea_list[row])です")
+    }
 }
 
